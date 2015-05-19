4846af8973cc068f10f61ec8e35c08a9bd69b71a
Juan Hayashi <juan.hayashi@usach.cl>
2015-05-19 18:21:55 -0300

    Actualizacion

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request
-
+import json
 app = Flask(__name__)
 
 @app.route('/datos', methods=['POST'])
 def recibir_datos():
-	print request.json
+	print request.json['saludo']
+	print request.json['numeros']
 	return "Hola desde Python!", 201
 
 if __name__ == "__main__":
--- server.rb
+++ server.rb
@@ -8,7 +8,8 @@ end
 post '/datos' do
   request.body.rewind  # in case someone already read it
   datos = JSON.parse request.body.read
-  puts "Hello #{datos['tweet']}!"
+  puts "Hello #{datos['saludo']}!"
+  puts "#{datos['numeros']}"
   "Hola desde Ruby/Sinatra"
 end
 
